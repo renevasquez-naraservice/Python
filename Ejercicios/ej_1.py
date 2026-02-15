cc123ebf950ad0191024dd28d44d1b84364cbfe9
Rene Alejandro Vásquez Vare <rene.vasquez@uab.edu.bo>
2026-02-15 10:33:13 -0400

    Ejercicio 1: Operaciones matemáticas
    Ejercicio de Varibles y Operaciones básicas en Python.

---
+++ Ejercicios/ej_1.py
@@ -0,0 +1,13 @@
+# Ejercicio de Variables y Operaciones básicas en Python}
+# Practicar con variables y operaciones básicas es fundamental para entender cómo funciona la programación en Python. A continuación, se presentan algunos ejercicios para ayudarte a mejorar tus habilidades
+# Enunciado => Pide al usuario 2 números y muestra la suma, resta, multiplicación y división de esos números.
+# Solicitar al usuario que ingrese dos números
+
+num1 = float(input("Ingrese el primer número: " ))
+num2 = float(input("Ingrese el segundo número: " ))
+
+# Imprimir los resultados de las operaciones básicas
+print ("Suma: ",num1 + num2)
+print ("resta: ",num1 - num2)
+print ("Multiplicacion: ", num1 * num2)
+print ("Division: ", num1 / num2)
