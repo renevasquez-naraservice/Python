960920f544ee14ad83309f8d538154bf7045146a
Rene Alejandro Vásquez Vare <rene.vasquez@uab.edu.bo>
2026-02-19 14:48:25 -0400

    Ejercicio 10: Condicional Simple
    Objetivo: Aprender If
    Enunciado: Determinar si un número entregado por el usuario es positivo o negativo.

---
+++ Ejercicios/ej_10.py
@@ -0,0 +1,9 @@
+# Ejercicio 10
+# Condicional Simple
+# Objetivo => Aprender if
+# Enunciado => Determinar si un número es positivo o negativo.
+numero = int(input("Ingrese un número entero: "))
+if numero >= 0:
+    print("El número es positivo")
+else:
+    print("El número es negativo")
