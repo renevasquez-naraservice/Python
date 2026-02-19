f35cf6709dcadfc53ac3d734b19f7b4d3c8a4ebe
Rene Alejandro Vásquez Vare <rene.vasquez@uab.edu.bo>
2026-02-19 15:07:58 -0400

    Ejercicio 13: Buble While
    Objetivo: Controlar ciclos con condición
    Enunciado:  Pide un número hasta que el usuario escriba 0 (cero).

---
+++ Ejercicios/ej_13.py
@@ -0,0 +1,9 @@
+# Ejercicio 13
+# Bucle while
+# Objetivo => Controlar ciclos con condición
+# Enunciado => Pide números hasta que el usuario escriba 0
+numero = 1
+while numero != 0:
+    numero = int (input("Ingresa un número: "))
+    print(f"Ingresaste {numero}")
+    print("<-------------------------------------->")
