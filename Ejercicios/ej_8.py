24786cf1f99f155e8de7963221f8836cb5196ef0
Rene Alejandro Vásquez Vare <rene.vasquez@uab.edu.bo>
2026-02-19 14:38:15 -0400

    Ejercicio 8: Variables y tipos de datos.
    Objetivo: Entender variables y tipos básicos.
    
    Ejercicio:
    Pide al usuario su nombre, edad y estatura. Luego imprime un mensaje personalizado.

---
+++ Ejercicios/ej_8.py
@@ -0,0 +1,7 @@
+# Ejercicio Número 8
+# Enunciado => Pide al usuario su nombre, edad y estatura. Luego imprime un mensaje personalizado (convierte a int la edad y usa float para estatura).
+# Tenemos
+nombre = input("Hola Usuario. Cuál es tu nombre?: ")
+edad = int(input("¿Cuántos años tienes?: "))
+estatura = float(input("Eres alto? Cuanto: "))
+print(f"Usted es {nombre}, tienes {edad} y una estatura de {estatura} mts. :)")
