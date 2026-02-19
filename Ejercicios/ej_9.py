b0de030583d06d632701ea201043453ee7bebae7
Rene Alejandro Vásquez Vare <rene.vasquez@uab.edu.bo>
2026-02-19 14:43:59 -0400

    Ejercicio 9: Operaciones matemáticas
    Objetivo: Practicar Operadores
    Enunciado: Pide un número y muestra
    +Su Doble
    +Su Triple
    +Su Cuadrado

---
+++ Ejercicios/ej_9.py
@@ -0,0 +1,8 @@
+# Ejercicio 9
+# Operaciones matemáticas
+# Objetivo => Practicar Operadores
+# Enunciado => Pide un número y muestra su doble, triple, su cuadrado
+numero  =   int(input("Ingresa un número: "))
+print (f"doble => {numero *2}")
+print (f"triple => {numero *3}")
+print (f"cuadrado => {numero **2}")
